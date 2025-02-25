7089c1e705ff218ba82d545d2c4a3287099f2f3e
Manda <mandashen@gmail.com>
2025-02-25 11:20:15 +0800

    複製檔案到此專案資料夾

---
+++ resources/views/student/index.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+
+<body>
+    @php
+        // dd($data);
+    @endphp
+
+    <div class="container mt-3">
+        <h2>Bordered Table</h2>
+        <p>The .table-bordered class adds borders on all sides of the table and the cells:</p>
+        <table class="table table-bordered">
+            <thead>
+                <tr>
+                    <th width="10%">ID</th>
+                    <th>NAME</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($data as $value)
+                    <tr>
+                        <td>{{ $value['id'] }}</td>
+                        <td>{{ $value['name'] }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+
+
+</body>
+
+</html>
---
+++ routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StudentController;
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+ 
+Route::get('/students_hello', [StudentController::class,'excel']);
+Route::get('/how_it_work', [StudentController::class,'viewcontroler']);
+Route::resource('students', StudentController::class);
